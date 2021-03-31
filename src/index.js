e18110be4242d4f31f29c324f5ecdc3e4b424a79
github username <github email address>
2021-03-30 19:20:34 -0500

    commit tree d8ccd1c16c8287483f3e00ea691e7952e856af82

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadImg();
+    loadBreeds();
+  })
+
+function loadImg() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        results.message.forEach(image => addImg(image))
+    });
+}
+
+function addImg(img) {
+    const container = document.getElementById("dog-image-container");
+    const image = document.createElement("img");
+
+    image.src = img;
+    container.appendChild(image);
+};
+
+function loadBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        const breeds = Object.keys(results.message);
+        addBreeds(breeds);
+       
+    })
+}
+
+
+function addBreeds(breeds) {
+    const ul = document.getElementById("dog-breeds");
+    breeds.forEach(breed => {
+      const li = document.createElement("li");
+      li.innerText = breed;
+      ul.appendChild(li);
+      li.addEventListener("click", function(event) {
+        event.target.style.color = "gold";
+      });
+    });
+}
+
+function filterBreeds(breed) {
+    
+}
